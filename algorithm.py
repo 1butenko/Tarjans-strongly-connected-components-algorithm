471615a6690f027455bd9ab908370ab1a07e09b7
1butenko <ihor.butenko@icloud.com>
2025-12-09 13:53:45 +0200

    fixed algorithm for adj list and adj matrix separately

--- algorithm.py
+++ algorithm.py
@@ -2,67 +2,80 @@ class TarjansSccAlgorithm:
     def __init__(self):
         self.index = 0
         self.S = []
-        self.V = None
-        self.E = None
+        self.graph = None
         self.on_stack = set()
         self.lowlink = {}
         self.indices = {}
         self.sccs = []
     
     def find_scc_adjacency_list(self, adj_list):
-        self.E = self._get_edges_adj_list(adj_list)
-        self.V = adj_list.keys()
+        self.graph = adj_list
+        self._reset()
         
-        for v in self.V:
+        for v in adj_list.keys():
             if v not in self.indices:
-                self._strongconnect(v)
+                self._strongconnect_list(v)
                 
-        result = self.sccs 
-        self.__init__()
-        return result
+        return self.sccs
     
     def find_scc_adjacency_matrix(self, adj_matrix):
-        self.E = self._get_edges_adj_matrix(adj_matrix)
-        self.V = list(range(adj_matrix.shape[0]))
+        self.graph = adj_matrix
+        self._reset()
 
-        for v in self.V:
+        n = len(adj_matrix)
+
+        for v in range(n):
             if v not in self.indices:
-                self._strongconnect(v)
+                self._strongconnect_matrix(v)
                 
-        result = self.sccs 
-        self.__init__()
-        return result
-
-    def _get_edges_adj_list(self, adj_list):
-        edges = set()
-
-        for k, v in adj_list.items():
-            for i in v:
-                edges.add((k, i))
-
-        return edges
+        return self.sccs
     
-    def _get_edges_adj_matrix(self, adj_matrix):
-        edges = set()
+    def _reset(self):
+        self.index = 0
+        self.S = []
+        self.on_stack = set()
+        self.lowlink = {}
+        self.indices = {}
+        self.sccs = []
 
-        for i, row in enumerate(adj_matrix):
-            for j, element in enumerate(row):
-                if element == 1:
-                    edges.add((i, j))
-
-        return edges
-
-    def _strongconnect(self, v):
+    def _strongconnect_list(self, v):
         self.indices[v] = self.index
         self.lowlink[v] = self.index
         self.index += 1
         self.S.append(v)
         self.on_stack.add(v)
 
-        for src, w in self.E:
-            if src == v:
+        for w in self.graph.get(v, []):
+            if w not in self.indices:
+                self._strongconnect_list(w)
+                self.lowlink[v] = min(self.lowlink[v], self.lowlink[w])
+            elif w in self.on_stack:
+                self.lowlink[v] = min(self.lowlink[v], self.indices[w])
+        
+        if self.lowlink[v] == self.indices[v]:
+            scc = set()
+
+            while True:
+                w = self.S.pop()
+                self.on_stack.remove(w)
+                scc.add(w)
+
+                if w == v:
+                    break
+
+            self.sccs.append(scc)
+
+    def _strongconnect_matrix(self, v):
+        self.indices[v] = self.index
+        self.lowlink[v] = self.index
+        self.index += 1
+        self.S.append(v)
+        self.on_stack.add(v)
+
+        for w, has_edge in enumerate(self.graph[v]):
+            if has_edge:
                 if w not in self.indices:
-                    self._strongconnect(w)
+                    self._strongconnect_matrix(w)
                     self.lowlink[v] = min(self.lowlink[v], self.lowlink[w])
                 elif w in self.on_stack:
                     self.lowlink[v] = min(self.lowlink[v], self.indices[w])
